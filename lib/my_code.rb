acd74a77c44d73c0c2d5b3b5af79ffff25194c88
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-18 00:05:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(yield (array[i]))
+    i += 1
+  end
+  return new
+end
+
+
+
+
+def reduce(source_array, starting_point=0) 
+  #{|memo, n| memo + n}
+  #{|memo, n| memo && n}
+  #{|memo, n| memo || n}
+  i = 0
+  total= starting_point
+  while i < source_array.length do
+    total = yield(total, source_array[i])
+    i += 1
+  end
+  return total unless
+    
+end
